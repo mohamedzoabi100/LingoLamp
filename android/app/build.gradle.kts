4df6b199a0ca21381fa839d2cf69356a0ec96f2d
Nabeeh <nabeeh.job@gmail.com>
2025-06-03 16:00:02 +0300

    Nabeeh Push

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.technion.android.lingo_lamp"
+    namespace = "com.technion.lingo_lamp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"  // CHANGE THIS LINE
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.technion.android.lingo_lamp"
+        applicationId = "com.technion.lingo_lamp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/lingolampmainpage/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/lingolampmainpage/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.technion.android.lingo_lamp
+package com.technion.lingo_lamp
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
